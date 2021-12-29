4d816eae7087fdce65e3ceddd6e0be2804510f86
amannnyadav <av2082@srmist.edu.in>
2021-12-29 21:44:50 +0530

    project

---
+++ server/app.js
@@ -0,0 +1,41 @@
+const dotenv=require("dotenv");
+const express=require ("express");
+const app= express();
+app.use(express.json());
+
+dotenv.config({path:"./config.env"});
+
+
+require("./db/conn");
+
+// const User=require('./model/userschema');
+
+app.use(require('./router/auth'));
+
+const PORT=process.env.PORT
+
+
+const middleware=(req,res,next)=>{
+    console.log("hello my middleware");
+    next();
+}
+// app.get("/",(req,res)=>{
+//     res.send("home page");
+// });
+
+app.get("/emp",(req,res)=>{
+    res.send("employee page");
+});
+
+app.get("/update",middleware,(req,res)=>{
+    res.send("update page");
+});
+
+
+app.get("/register",(req,res)=>{
+    res.send("register page");
+});
+
+app.listen(PORT,()=>{
+    console.log("connection successful")
+});
---
+++ server/router/auth.js
@@ -0,0 +1,49 @@
+const express=require("express");
+const router=express.Router();
+require("../db/conn");
+const User=require("../model/userschema");
+
+router.get("/employees",(req,res)=>{
+    res.send("home router page");
+});
+
+// router.post("/register",  (req,res)=>{
+//     const {name,contact,email,designation,department}=req.body;
+//     if (!name || !contact || !email || !designation || !department){
+//         return res.status(422).json({error:"plz fill the field properly"});
+//     }
+//     User.findOne({email:email})
+//     .then((userExist)=>{
+//         if (userExist){
+//             return res.status(422).json({error:"email already exists"});
+//         }
+
+//         const user=new User({name,contact,email,designation,department});
+//         user.save().then(()=>{
+//             res.status(201).json({message:'user registered successfully'});
+//         }).catch((err)=> res.status(500).json({error:"failed to registered"}));
+//     }).catch(err =>{console.log(err);});
+    
+// });
+
+//async=await
+router.post("/register", async (req,res)=>{
+    const {Name,Contact,Email,Designation,Department}=req.body;
+    if (!Name || !Contact || !Email || !Designation || !Department){
+        return res.status(422).json({error:"plz fill the field properly"});
+    }
+    try{
+        const userExist=await User.findOne({Email:Email});
+        if (userExist){
+            return res.status(422).json({error:"Email already exist"});
+        }
+        const user=new User({Name,Contact,Email,Designation,Department});
+        await user.save();
+        res.status(201).json({message:'user registered successfully'});
+    }
+    catch(error){
+        console.log(error);
+    }
+});
+
+module.exports=router;
